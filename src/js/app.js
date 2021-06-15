223bc5a129a23f670fff9cee73b25df8efc47e91
Biggs Thorarensen <biggs@devillion.com>
2021-06-15 10:17:28 +0100

    fetching data and rendering name, likes and body of comments.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,28 @@
 const Utils = require('./utils');
 
 new Utils();
+
+fetch('https://my-json-server.typicode.com/telegraph/front-end-exercise-contractors/comments')
+    .then(res => res.json())
+    .then(data => {
+      loadComments(data)
+    })
+    .catch(err => {
+        console.log(`Error with message: ${err}`)
+    });
+
+function loadComments(data) {
+  const container = document.getElementById("comments")
+  const listEl = document.createElement("ul")
+  container.appendChild(listEl)
+  for (var i = 0; i < data.length; i++) {
+    const listItemEl = document.createElement("li")
+    listItemEl.insertAdjacentHTML(
+      'afterbegin',
+      `<h3>${data[i].name}</h3>
+      <span>${data[i].likes}</span>
+      <p>${data[i].body}</p>`
+    )
+    listEl.appendChild(listItemEl)
+  }
+}
